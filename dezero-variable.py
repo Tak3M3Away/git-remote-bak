0a40cd63f7a11e1308135a196b084d965591d4d4
Tak3M3Away <mairusandoria@gmail.com>
2022-12-14 10:31:35 +0900

    the first commit

---
+++ dezero-variable.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = x ** 2
+        output = Variable(y)
+        return output
+
+
+data = np.array(1.0)
+x = Variable(data)
+print(x.data)
+
+x.data = np.array(2.0)
+print(x.data)
+
+x = Variable(np.array(10))
+f = Function()
+y = f(x)
+
+print(type(y))
+print(y.data)
+
+print(x.data)
